c06472ced75583c91f6de1e60081c3b9eef9bd56
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-18 01:07:29 +0100

    Spring-Boot Framework Practice

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/model/Mentor.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/model/Mentor.java
@@ -9,6 +9,12 @@ import lombok.*;
 @ToString
 public class Mentor {
 
-
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private boolean graduated;
+    private String batch;
+    private String company;
 
 }
