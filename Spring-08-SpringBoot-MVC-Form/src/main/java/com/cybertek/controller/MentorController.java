1d239b296409c50e426966342c8dd90bd07ee920
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-18 02:50:07 +0100

    Spring-Boot Framework Practice

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -4,6 +4,7 @@ import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,7 +28,9 @@ public class MentorController {
     }
 
     @PostMapping("/confirm")
-    public String submitForm(){
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+
+        System.out.println(mentor.toString());
 
         return "mentor/mentor-confirmation";
 
