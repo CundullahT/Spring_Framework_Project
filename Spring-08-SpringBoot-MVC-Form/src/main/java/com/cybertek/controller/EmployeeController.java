80935b5cef73ee066d37b9472992e23dc75f020a
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-19 00:46:38 +0100

    Spring-Boot Framework Practice

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/register")
+    public String employeeCreate(){
+
+        return "employee/employee-create";
+
+    }
+
+}
---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/model/Employee.java
@@ -0,0 +1,23 @@
+package com.cybertek.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
