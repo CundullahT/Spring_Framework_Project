14e6b958da457a1d39cbb99f1fd0899e175c145b
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-19 01:46:48 +0100

    Spring-Boot Framework Practice

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String employeeCreate(){
+    public String employeeCreate(Model model){
+
+        model.addAttribute("employee", new Employee());
+
 
         return "employee/employee-create";
 
---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/datagenerator/DataGenerator.java
@@ -0,0 +1,66 @@
+package com.cybertek.datagenerator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<String> getStateList(){
+
+        List<String> stateList = Arrays.asList(
+                "Alaska",
+                "Alabama",
+                "Arkansas",
+                "Arizona",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Iowa",
+                "Idaho",
+                "Illinois",
+                "Indiana",
+                "Kansas",
+                "Kentucky",
+                "Louisiana",
+                "Massachusetts",
+                "Maryland",
+                "Maine",
+                "Michigan",
+                "Minnesota",
+                "Missouri",
+                "Mississippi",
+                "Montana",
+                "NorthCarolina",
+                "NorthDakota",
+                "Nebraska",
+                "NewHampshire",
+                "NewJersey",
+                "NewMexico",
+                "Nevada",
+                "NewYork",
+                "Ohio",
+                "Oklahoma",
+                "Oregon",
+                "Pennsylvania",
+                "RhodeIsland",
+                "SouthCarolina",
+                "SouthDakota",
+                "Tennessee",
+                "Texas",
+                "Utah",
+                "Virginia",
+                "Vermont",
+                "Washington",
+                "Wisconsin",
+                "WestVirginia",
+                "Wyoming");
+
+        return stateList;
+
+    }
+
+}
