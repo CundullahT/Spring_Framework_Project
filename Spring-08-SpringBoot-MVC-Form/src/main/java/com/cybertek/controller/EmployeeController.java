9aedaeb29221223aa91ae6207057fafef16145be
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-19 01:57:56 +0100

    Spring-Boot Framework Practice

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.cybertek.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +19,13 @@ public class EmployeeController {
         model.addAttribute("stateList", DataGenerator.getStateList());
 
         return "employee/employee-create";
+
+    }
+
+    @PostMapping("/list")
+    public String employeeList(){
+
+        return "employee/employee-list";
 
     }
 
