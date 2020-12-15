ff688929b9214e824b4def3e934b48da3bcbe4f0
CundullahT <cundullahterzioglu.it@gmail.com>
2020-12-15 22:26:18 +0100

    Spring-Boot Framework Practice

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -9,8 +9,17 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "studentFirstName")
     private String firstName;   //first_name
+
+    @Column(name = "studentLastName")
     private String lastName;
+
+    @Column(name = "studentEmailAddress")
     private String email;
+
+    @Transient
+    private String city;
 
 }
--- Spring-09-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO students(first_name, last_name, email)
+INSERT INTO students(student_first_name, student_last_name, student_email_address)
 VALUES ('Mike', 'Smith', 'mike@cybertek.com');
-INSERT INTO students(first_name, last_name, email)
+INSERT INTO students(student_first_name, student_last_name, student_email_address)
 VALUES ('Adam', 'Shaun', 'adam@cybertek.com');
-INSERT INTO students(first_name, last_name, email)
+INSERT INTO students(student_first_name, student_last_name, student_email_address)
 VALUES ('Tom', 'Hanks', 'tom@cybertek.com');
-INSERT INTO students(first_name, last_name, email)
+INSERT INTO students(student_first_name, student_last_name, student_email_address)
 VALUES ('Brad', 'Kelly', 'brad@cybertek.com');
