10dfeab7e6a987ded91dae6a7eb4b4a7f76102cd
CundullahT <cundullahterzioglu.it@gmail.com>
2020-12-17 05:22:09 +0100

    Spring-Boot Framework Practice

--- Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/cybertek/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cybertek.repository;
-
-import com.cybertek.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-}
