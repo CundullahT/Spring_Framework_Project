83eaf9696a076b62646fd319a64d158ab594c2a9
CundullahT <cundullahterzioglu.it@gmail.com>
2020-12-17 05:07:34 +0100

    Spring-Boot Framework Practice

--- Spring-09-ORM-Mapping-OneToOne/src/main/java/com/cybertek/entity/Employee.java
+++ Spring-09-ORM-Mapping-OneToOne/src/main/java/com/cybertek/entity/Employee.java
@@ -31,6 +31,10 @@ public class Employee extends BaseEntity {
     @JoinColumn(name = "department_id")
     private Department department;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
     public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
         this.firstName = firstName;
         this.lastName = lastName;
