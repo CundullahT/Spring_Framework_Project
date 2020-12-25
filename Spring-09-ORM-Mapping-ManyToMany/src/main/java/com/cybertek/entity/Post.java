8d1d212ba487bd78fd5b8e4bed41c942b694fc5c
CundullahT <cundullahterzioglu@gmail.com>
2020-12-25 05:26:00 +0100

    Initial Commit.

---
+++ Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Post.java
@@ -0,0 +1,32 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+
+
+    public Post(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+}
