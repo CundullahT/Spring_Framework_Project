04aaf38af6cd631a80070b906be07f1163e95f11
CundullahT <cundullahterzioglu@gmail.com>
2020-12-31 04:38:57 +0100

    Initial Commit.

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/User.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/User.java
@@ -5,10 +5,29 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "user_account")
 public class User extends BaseEntity {
+
+    private String email;
+    private String password;
+    private String username;
+
+    @OneToOne
+    @JoinColumn(name = "account_details_id")
+    private Account account;
+
+    public User(String email, String password, String username) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
 }
