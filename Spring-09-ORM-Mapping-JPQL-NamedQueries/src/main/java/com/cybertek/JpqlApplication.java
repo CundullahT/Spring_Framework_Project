107c65f01945dc8b1d97a5c3429f99f72f184cb2
CundullahT <cundullahterzioglu@gmail.com>
2021-01-02 05:31:05 +0100

    Initial Commit.

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/JpqlApplication.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/JpqlApplication.java
@@ -1,13 +1,28 @@
 package com.cybertek;
 
+import com.cybertek.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JpqlApplication {
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(JpqlApplication.class, args);
+    }
+
+    @PostConstruct
+    public void testEmployee(){
+
+        System.out.println(employeeRepository.getEmployeeDetail());
+        System.out.println(employeeRepository.getEmployeeSalary());
+
     }
 
 }
--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -2,11 +2,16 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    @Query("SELECT e FROM Employee e WHERE e.email = 'dtrail8@tamu.edu'")
+    Employee getEmployeeDetail();
 
+    @Query("SELECT e.salary FROM Employee e WHERE e.email = 'dtrail8@tamu.edu'")
+    Integer getEmployeeSalary();
 
 }
