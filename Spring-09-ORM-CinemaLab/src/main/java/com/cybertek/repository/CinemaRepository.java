acea2766d5ffe1078591243f909e94dda8021ef6
CundullahT <cundullahterzioglu@gmail.com>
2021-01-03 03:27:45 +0100

    Initial Commit.

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -17,7 +17,7 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     Optional<Cinema> findByName(String name);
 
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
-    List<Cinema> findFirst3BySponsoredNameContainingOrderBySponsoredNameAsc(String sponsoredname);
+    List<Cinema> findFirst3BySponsoredNameContainingOrderBySponsoredNameAsc(String sponsoredName);
 
     //Write a derived query to list all cinemas in a specific country
     List<Cinema> findAllByLocationCountry(String country);
