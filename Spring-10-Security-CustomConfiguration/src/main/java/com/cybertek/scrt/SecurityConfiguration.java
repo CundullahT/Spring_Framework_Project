03a5f04a9a351a6ea986076cbe363defac59a8a5
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-20 19:17:16 +0100

    Initial Commit.

--- Spring-10-Security-CustomConfiguration/src/main/java/com/cybertek/scrt/SecurityConfiguration.java
+++ Spring-10-Security-CustomConfiguration/src/main/java/com/cybertek/scrt/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.cybertek.scrt;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -23,9 +26,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password("admin123").roles("ADMIN")
+                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
                 .and()
-                .withUser("ozzy").password("ozzy123").roles("USER");
+                .withUser("jundy").password(passwordEncoder().encode("jundy123")).roles("USER");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
