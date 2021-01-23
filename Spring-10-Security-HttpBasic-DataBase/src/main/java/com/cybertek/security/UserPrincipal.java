8fae0c63a7a99a1d5d4ce9d18e636912fb3e15b8
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-23 12:50:35 +0100

    Initial Commit.

---
+++ Spring-10-Security-HttpBasic-DataBase/src/main/java/com/cybertek/security/UserPrincipal.java
@@ -0,0 +1,71 @@
+package com.cybertek.security;
+
+import com.cybertek.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        // Extract list of permissions
+        user.getPermissionList().forEach(p -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority(p);
+            authorityList.add(authority);
+        });
+
+        // Extract list of roles
+        user.getRoleList().forEach(r -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + r);
+            authorityList.add(authority);
+        });
+
+        return authorityList;
+
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.getActive() == 1;
+    }
+
+}
