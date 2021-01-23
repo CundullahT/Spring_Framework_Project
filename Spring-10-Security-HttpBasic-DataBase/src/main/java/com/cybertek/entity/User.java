920bbaa020cf5084277b38e17d3d088e9d70cee7
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-23 08:02:36 +0100

    Initial Commit.

---
+++ Spring-10-Security-HttpBasic-DataBase/src/main/java/com/cybertek/entity/User.java
@@ -0,0 +1,23 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private int active;
+    private String roles;
+
+}
