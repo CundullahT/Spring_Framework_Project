7454f552f93af8c8d630ff4c29ecadc0e4620d92
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-28 12:48:34 +0100

    Initial Commit.

--- Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/controller/ProductController.java
@@ -4,7 +4,11 @@ import com.cybertek.entity.Product;
 import com.cybertek.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -15,8 +19,14 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)   // In default it is already method = RequestMethod.GET
     public @ResponseBody Product getProduct(@PathVariable("id") Long id) throws Exception {
         return productService.getProduct(id);
+    }
+
+    @RequestMapping(value = "/products")
+    public @ResponseBody List<Product> getProduct(){
+        return productService.getProducts();
     }
 
 }
