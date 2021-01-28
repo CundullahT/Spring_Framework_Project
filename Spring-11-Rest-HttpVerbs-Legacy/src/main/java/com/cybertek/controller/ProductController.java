886a4f0b7e2ff7c0acb8a15fa985e4199b5a5597
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-28 12:36:34 +0100

    Initial Commit.

--- Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.cybertek.controller;
 
+import com.cybertek.entity.Product;
+import com.cybertek.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public @ResponseBody Product getProduct(@PathVariable("id") Long id) throws Exception {
+        return productService.getProduct(id);
+    }
+
 }
