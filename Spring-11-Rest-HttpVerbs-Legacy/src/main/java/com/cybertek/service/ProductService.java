d02ccceea655c62bdf17a6ccc5c621928cf80a15
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-28 12:29:25 +0100

    Initial Commit.

--- Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/service/ProductService.java
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/service/ProductService.java
@@ -8,8 +8,8 @@ public interface ProductService {
 
     List<Product> getProducts();
     List<Product> delete(long id);
-    List<Product> updateProduct(long id, Product product);
-    List<Product> createProduct(Product product);
-    Product getProduct(long id);
+    List<Product> updateProduct(long id, Product product) throws Exception;
+    List<Product> createProduct(Product product) throws Exception;
+    Product getProduct(long id) throws Exception;
 
 }
