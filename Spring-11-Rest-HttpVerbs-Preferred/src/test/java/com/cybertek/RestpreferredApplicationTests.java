e796670ce07ec6af6fe8dd8bafdbbb5961580780
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-30 04:38:04 +0100

    Initial Commit.

---
+++ Spring-11-Rest-HttpVerbs-Preferred/src/test/java/com/cybertek/RestpreferredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestpreferredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
