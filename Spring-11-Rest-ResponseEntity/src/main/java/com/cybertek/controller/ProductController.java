7727ddbbb373bb893a468e7204ed456cf9ece9b9
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-30 13:30:39 +0100

    Initial Commit.

--- Spring-11-Rest-ResponseEntity/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-Rest-ResponseEntity/src/main/java/com/cybertek/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.cybertek.controller;
 import com.cybertek.entity.Product;
 import com.cybertek.service.ProductService;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,8 +40,15 @@ public class ProductController {
     }
 
     @PostMapping
-    public List<Product> createProduct(@RequestBody Product product) throws Exception {
-        return productService.createProduct(product);
+    public ResponseEntity<List<Product>> createProduct(@RequestBody Product product) throws Exception {
+
+        List<Product> set = productService.createProduct(product);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version", "Cybertek.v1")
+                .header("Operation", "Create")
+                .body(set);
     }
 
     @DeleteMapping(value = "/{id}")
