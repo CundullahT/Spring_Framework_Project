bc8f69aa07f13c07492e2b590d94ff0293cd17ce
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-30 15:48:09 +0100

    Initial Commit.

--- Spring-11-Rest-Jackson/src/main/java/com/cybertek/entity/Genre.java
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/entity/Genre.java
@@ -1,5 +1,9 @@
 package com.cybertek.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,11 +17,14 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(value = {"hibernate_Lazy_Initializer"}, ignoreUnknown = true)
 public class Genre extends BaseEntity {
 
     private String name;
 
     @ManyToMany(mappedBy = "genreList")
+    @JsonIgnore
     private List<Movie> movieList = new ArrayList<>();
 
     public Genre(String name) {
