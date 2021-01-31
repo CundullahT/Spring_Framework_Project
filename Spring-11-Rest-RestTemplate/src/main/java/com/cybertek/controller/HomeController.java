1945bfe495b8a8c3eb2d84da7d23f84b5a423de0
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-31 18:41:04 +0100

    Initial Commit.

--- Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.cybertek.controller;
 
 import com.cybertek.entity.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @RestController
 public class HomeController {
@@ -29,6 +31,20 @@ public class HomeController {
         String URL = URI + "/{id}";
         return restTemplate.getForObject(URL, Object.class, id);        // You can put any kind of object here. For example: User.class.
                                                                         // If you use it like this you won't be able to use the constraints you put in User class.
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostsFromDummyApi(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id", "lTE5abbDxdjGplutvTuc");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/api/user?limit=10", HttpMethod.GET, entity, Object.class);
+
+        return response;
+
     }
 
 }
