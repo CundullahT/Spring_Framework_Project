8537b60c5f7716b41e0860c4b95ab50e59c949ee
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-31 18:18:48 +0100

    Initial Commit.

--- Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.cybertek.controller;
 import com.cybertek.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +22,13 @@ public class HomeController {
     public User[] readAllUsers(){
         ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI, User[].class);
         return responseEntity.getBody();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Object readUser(@PathVariable("id") Integer id){
+        String URL = URI + "/{id}";
+        return restTemplate.getForObject(URL, Object.class, id);        // You can put any kind of object here. For example: User.class.
+                                                                        // If you use it like this you won't be able to use the constraints you put in User class.
     }
 
 }
