60ffafedffa9617c928a8f6cff234b822fb0bf36
CundullahT <cundullahterzioglu.it@gmail.com>
2021-01-31 17:17:24 +0100

    Initial Commit.

--- Spring-11-Rest-Jackson/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.cybertek.controller;
 
+import com.cybertek.entity.Account;
 import com.cybertek.entity.User;
+import com.cybertek.repository.AccountRepository;
 import com.cybertek.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,21 @@ import java.util.List;
 public class HomeController {
 
     private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
 
-    public HomeController(UserRepository userRepository) {
+    public HomeController(UserRepository userRepository, AccountRepository accountRepository) {
         this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
     }
 
     @GetMapping("/users")
     public List<User> readAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/accounts")
+    public List<Account> readAllAccounts(){
+        return accountRepository.findAll();
     }
 
 }
