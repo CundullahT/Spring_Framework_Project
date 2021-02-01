6ea78ba95eef48a50e6d2ba28544df9f7acff0d2
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-01 12:34:57 +0100

    Initial Commit.

---
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/enums/AddressType.java
@@ -0,0 +1,7 @@
+package com.cybertek.enums;
+
+public enum AddressType {
+
+    STUDENT, TEACHER, PARENT
+
+}
---
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/enums/Status.java
@@ -0,0 +1,7 @@
+package com.cybertek.enums;
+
+public enum Status {
+
+    ACTIVE, FINISHED, PENDING, MOVED
+
+}
