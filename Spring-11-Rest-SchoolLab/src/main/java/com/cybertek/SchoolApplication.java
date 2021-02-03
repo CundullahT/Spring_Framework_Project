e6b154949fd31db866991d21ea0a6f04bc4c795a
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-03 20:17:23 +0100

    Initial Commit.

--- Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/SchoolApplication.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/SchoolApplication.java
@@ -2,12 +2,19 @@ package com.cybertek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SchoolApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
