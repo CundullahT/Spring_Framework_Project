4a30f0a3a6c6f1fd66266a071276cb45ed9fbf6f
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-03 22:56:24 +0100

    Initial Commit.

--- Spring-11-Rest-Jackson/src/main/java/com/cybertek/controller/WebFluxController.java
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/controller/WebFluxController.java
@@ -1,10 +1,36 @@
 package com.cybertek.controller;
 
+import com.cybertek.entity.MovieCinema;
+import com.cybertek.repository.GenreRepository;
+import com.cybertek.repository.MovieCinemaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class WebFluxController {
 
+    private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
 
+    private final MovieCinemaRepository movieCinemaRepository;
+    private final GenreRepository genreRepository;
+
+    public WebFluxController(MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.genreRepository = genreRepository;
+    }
+
+    @GetMapping("/flux-movie-cinemas")
+    public Flux<MovieCinema> readAllCinemaFlux(){
+        return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
+
+    @GetMapping("/mono-movie-cinema/{id}")
+    public Mono<MovieCinema> readCinemaByIdMono(@PathVariable("id") Long id){
+        return Mono.just(movieCinemaRepository.findById(id).get());
+    }
 
 }
