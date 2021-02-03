8bc97ac68c66e3b67295416a330ac1ef22ad10d7
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-03 20:14:07 +0100

    Initial Commit.

--- Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -2,8 +2,10 @@ package com.cybertek.controller;
 
 import com.cybertek.model.ResponseWrapper;
 import com.cybertek.model.Teacher;
+import com.cybertek.repository.ParentRepository;
 import com.cybertek.repository.StudentRepository;
 import com.cybertek.repository.TeacherRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,12 @@ public class ApiController {
 
     private final TeacherRepository teacherRepository;
     private final StudentRepository studentRepository;
+    private final ParentRepository parentRepository;
 
-    public ApiController(TeacherRepository teacherRepository, StudentRepository studentRepository) {
+    public ApiController(TeacherRepository teacherRepository, StudentRepository studentRepository, ParentRepository parentRepository) {
         this.teacherRepository = teacherRepository;
         this.studentRepository = studentRepository;
+        this.parentRepository = parentRepository;
     }
 
     @GetMapping("/teachers")
@@ -29,6 +33,14 @@ public class ApiController {
     @GetMapping("/students")
     public ResponseEntity<ResponseWrapper> readAllStudents(){
         return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved", studentRepository.findAll()));
+    }
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+        ResponseWrapper responseWrapper = new ResponseWrapper(true, "Parents are successfully retrieved",
+                HttpStatus.ACCEPTED.value(),
+                parentRepository.findAll());
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseWrapper);
     }
 
 }
