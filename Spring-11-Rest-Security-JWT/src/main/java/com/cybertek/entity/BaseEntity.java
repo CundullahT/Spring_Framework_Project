2ad43664fe780f4f93abff485bedf3e132dc0180
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-08 23:35:27 +0100

    Initial Commit.

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/entity/BaseEntity.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/entity/BaseEntity.java
@@ -1,9 +1,11 @@
 package com.cybertek.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @MappedSuperclass
 @Getter
@@ -16,5 +18,37 @@ public class BaseEntity {
 
     @Column(name = "is_deleted")
     private Boolean isDeleted;
+
+    @JsonIgnore
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @JsonIgnore
+    @Column(name = "updated_date", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
+
+    @JsonIgnore
+    @Column(name = "user_created", updatable = false)
+    private Integer createUserId;
+
+    @JsonIgnore
+    @Column(name = "user_updated", nullable = false)
+    private Integer updateUserId;
+
+    @PrePersist
+    private void onPrePersist(){
+        this.createdDate = new Date();
+        this.updatedDate = new Date();
+        this.createUserId = 1;
+        this.updateUserId = 1;
+    }
+
+    @PreUpdate
+    private void onPreUpdate(){
+        this.updatedDate = new Date();
+        this.updateUserId = 1;
+    }
 
 }
