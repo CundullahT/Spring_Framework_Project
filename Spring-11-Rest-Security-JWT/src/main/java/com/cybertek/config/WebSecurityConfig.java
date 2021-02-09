28869d10fb2f2d2727edba3aa8eef293b9130a7e
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-09 04:16:21 +0100

    Initial Commit.

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/config/WebSecurityConfig.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/config/WebSecurityConfig.java
@@ -1,15 +1,23 @@
 package com.cybertek.config;
 
+import com.cybertek.services.SecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final SecurityFilter securityFilter;
+
+    public WebSecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
 
     @Override
     @Bean
@@ -19,6 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .csrf()
                 .disable()
@@ -27,6 +36,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+
+        http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
 
 }
