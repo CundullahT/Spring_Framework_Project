a13060fc7c2eb750f7a1ba51beaabaf7f51e6607
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-09 03:46:49 +0100

    Initial Commit.

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/util/JWTUtil.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/util/JWTUtil.java
@@ -1,6 +1,7 @@
 package com.cybertek.util;
 
 import com.cybertek.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JWTUtil {
@@ -36,6 +38,17 @@ public class JWTUtil {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))    //  10 hours token validity.
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
+
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private <T> T extraClaim(String token, Function<Claims, T> claimsResolver){
+
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
 
     }
 
