52682762b4c8bc3f4780cba7ab2f2a61428c2672
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-10 02:54:34 +0100

    Initial Commit.

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
@@ -4,6 +4,8 @@ import com.cybertek.entity.ResponseWrapper;
 import com.cybertek.entity.User;
 import com.cybertek.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UserController {
 
     private final UserService userService;
@@ -21,6 +24,8 @@ public class UserController {
     }
 
     @GetMapping("/read")
+//    @PreAuthorize("hasAuthority('USER')")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
     public ResponseEntity<ResponseWrapper> readAll(){
         List<User> users = userService.getAll();
         return ResponseEntity.ok(new ResponseWrapper("Done!", users));
