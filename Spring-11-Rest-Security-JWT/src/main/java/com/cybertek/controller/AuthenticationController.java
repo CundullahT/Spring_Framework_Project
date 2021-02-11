dbecd338d09ad0fa6472beca7b6579968465ee3c
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-11 01:49:20 +0100

    Initial Commit.

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/AuthenticationController.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.cybertek.controller;
 
+import com.cybertek.annotation.DefaultExceptionMessage;
 import com.cybertek.entity.AuthenticationRequest;
 import com.cybertek.entity.ResponseWrapper;
 import com.cybertek.entity.User;
@@ -26,6 +27,7 @@ public class AuthenticationController {
     }
 
     @PostMapping("/authenticate")
+    @DefaultExceptionMessage(defaultMessage = "Bad Credentials!")
     public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest){
 
         String password = authenticationRequest.getPassword();
