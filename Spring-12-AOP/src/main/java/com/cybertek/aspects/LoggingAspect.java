918a9f662517f53223f418cf3e8dba9914191a0d
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-01 00:41:19 +0100

    Initial Commit.

---
+++ Spring-12-AOP/src/main/java/com/cybertek/aspects/LoggingAspect.java
@@ -0,0 +1,12 @@
+package com.cybertek.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class LoggingAspect {
+
+
+
+}
