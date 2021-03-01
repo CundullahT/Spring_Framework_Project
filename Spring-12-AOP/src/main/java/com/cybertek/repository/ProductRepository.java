7612ae082c2241bf60426173ffda91129e9ecf12
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-01 01:18:26 +0100

    Initial Commit.

--- Spring-12-AOP/src/main/java/com/cybertek/repository/ProductRepository.java
+++ Spring-12-AOP/src/main/java/com/cybertek/repository/ProductRepository.java
@@ -4,6 +4,11 @@ import com.cybertek.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Override
+    Optional<Product> findById(Long aLong);
 }
