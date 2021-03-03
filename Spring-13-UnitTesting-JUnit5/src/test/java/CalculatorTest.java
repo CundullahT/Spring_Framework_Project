6246e893f24f69da03bf5ace51a862b80f95501e
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-03 22:25:11 +0100

    Initial Commit.

--- Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -5,6 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
+    void testCase1(){
+
+    }
+
+    @Test
+    void testCase2(){
+
+    }
+
+    @Test
     void add() {
     }
+
 }
