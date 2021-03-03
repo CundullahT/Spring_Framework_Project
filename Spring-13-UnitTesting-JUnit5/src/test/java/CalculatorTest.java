2cf121ca6fccb8b634f0adb94bd7d71d04cf124d
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-03 22:30:09 +0100

    Initial Commit.

--- Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -6,7 +6,7 @@ class CalculatorTest {
 
     @Test
     void testCase1(){
-        fail("Not implemented yet.");
+        assertEquals(20, 20);
     }
 
     @Test
@@ -16,7 +16,8 @@ class CalculatorTest {
 
     @Test
     void add() {
-        fail("Not implemented yet.");
+        int actual = Calculator.add(2, 3);
+        assertEquals(5, actual);
     }
 
 }
