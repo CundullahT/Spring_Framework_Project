fe9f6ba634c979daa47f4fb296cca803146bd3e9
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-03 23:02:20 +0100

    Initial Commit.

---
+++ Spring-13-UnitTesting-JUnit5/src/main/java/CheckingAccount.java
@@ -0,0 +1,37 @@
+public class CheckingAccount {
+
+    double balance;
+    long accountNumber;
+    String accountHolder;
+    String type = "Checking";
+
+    public void setInfo(double pBalance, long pAccNumber, String pAccHolder) {
+        balance = pBalance;
+        accountNumber = pAccNumber;
+        accountHolder = pAccHolder;
+    }
+
+    public double deposit(double amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public double withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient fund in account");
+        }
+        return balance;
+    }
+
+    public double purchase(String item, double price) {
+        if (price <= balance) {
+            balance -= price;
+        } else {
+            balance -= (price + 35);
+        }
+        return balance;
+    }
+
+}
