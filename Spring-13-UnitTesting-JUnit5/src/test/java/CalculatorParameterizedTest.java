6850f3072a04ad863d8337e2a74dc60632e05270
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-03 23:44:55 +0100

    Initial Commit.

--- Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
+++ Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorParameterizedTest {
@@ -14,6 +17,15 @@ public class CalculatorParameterizedTest {
     @ValueSource(ints = {3, 6, 9})
     void testCase2(int number) {
         Assertions.assertEquals(0, number % 3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "JS", "TS"})
+//    @EmptySource
+//    @NullSource
+    @NullAndEmptySource
+    void testCase2(String args) {
+        Assertions.assertTrue(!args.isEmpty());
     }
 
 }
