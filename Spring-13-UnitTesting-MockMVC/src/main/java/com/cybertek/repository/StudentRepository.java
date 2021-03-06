b088ec3814db05b4d230b49ae983200351d44228
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-06 17:44:30 +0100

    Initial Commit.

---
+++ Spring-13-UnitTesting-MockMVC/src/main/java/com/cybertek/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
